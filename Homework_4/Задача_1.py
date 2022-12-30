2c94c831b3598ac8107eac40193b7c21ad74a47e
Olga Rudina <krabee@mail.ru>
2022-12-31 00:25:41 +0400

    Fix filepath

--- Homework_4/Задача_1.py
+++ Homework_4/Задача_1.py
@@ -3,7 +3,7 @@
 # Пример:
 # если k = 2, то многочлены могут быть => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
 
-import random 
+import random, os
 
 def create_my_dict():
     my_dict = {}
@@ -14,32 +14,33 @@ def create_my_dict():
                 koef = random.randint(0, 100)
                 if koef != 0:
                     break
-                # else:
-                #     print('Был 0')
             my_dict[i] = koef
         else:
-            my_dict[i] = random.randint(0, 100)
+            my_dict[i] = random.randint(0, 100)    
+    print(str(my_dict))
     return my_dict
 
-def get_equation(my_dict1):
+def get_equation(my_dict):
     equation =''
-    for item in my_dict1:
+    for item in my_dict:
         value = ''
-        if(my_dict1[item] == 0):
+        if(my_dict[item] == 0):
             continue    
-        if(my_dict1[item] == 1):
+        if(my_dict[item] == 1):
             value = ''
         else:
-            value = str(my_dict1[item])
+            value = str(my_dict[item])
 
         x = ''
-        if(item == 0 and my_dict1[item] == 1):
+        if(item == 0 and my_dict[item] == 1):
             x = '1'
         elif(item == 0):
             x = ''
+        elif(item == 1 and my_dict[item] != 1):
+            x = '*x'
         elif(item == 1):
             x = 'x'
-        elif(my_dict1[item] == 1):
+        elif(my_dict[item] == 1):
              x = 'x**' + str(item)
         else:
             x = '*x**' + str(item)
@@ -48,29 +49,22 @@ def get_equation(my_dict1):
             equation = equation + ' + ' + value + x
         else: 
             equation = value + x
-        print(equation)
-    equation = equation + ' = 0'
+    equation = equation + ' = 0'    
+    print(equation)    
     return equation
 
 def write_file(filename, value):
-    f = open(filename, "w")
+    f = open(os.path.join(os.path.dirname(__file__), filename), "w")
     f.write(value)
     f.close()
 
 #equation1
-my_dict1 = create_my_dict()
-print(str(my_dict1))
-equation = get_equation(my_dict1)
-print(equation)
+my_dict = create_my_dict()
+equation = get_equation(my_dict)
 write_file("equation1", equation)
 
 #equation2
-my_dict1 = create_my_dict()
-print(str(my_dict1))
-equation = get_equation(my_dict1)
-print(equation)
+my_dict = create_my_dict()
+equation = get_equation(my_dict)
 write_file("equation2", equation)
-
-
-
 
